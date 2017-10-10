242c5494782fe920f9381c9c5de9e7f2aef5c912
Rashi Bose <rbose@Rashis-MacBook-Pro.local>
2017-10-10 09:27:29 -0700

    Initial Commit

---
+++ SegueExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueExample
+//
+//  Created by Rashi Bose on 10/10/17.
+//  Copyright © 2017 Rashi Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
